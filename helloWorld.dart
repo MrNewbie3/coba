486729ea9feee863ee2b0312e11c6b66696d6f4e
Mr.Newbie <atharafiaffandy@gmail.com>
2022-02-23 21:52:18 +0700

    stringOperation

--- helloWorld.dart
+++ helloWorld.dart
@@ -3,7 +3,7 @@ void main (){
   /* MultiLine Comment */ 
   var name = "Dart";
   // str
-  String coba = "Mencoba dart";
+  String coba = "Mencoba dart bersama Atharafi";
   // int
   int angka = 12;
   // Map
@@ -16,12 +16,16 @@ void main (){
   // boolean
   var bool = true;
   print(coba.contains("Dart"));
+  print(coba.toLowerCase());
+  print(coba.toUpperCase());
+  print(coba.split(" "));
+  print(angka.toString());
+  print(coba.substring(5, 10));
   print(coba + " "+ name +" Ini ${angka}" );
   print(flyobj[1]);
   print(object);
   print(bool);
-  print(coba.toLowerCase());
-  
+
 }
 
 
